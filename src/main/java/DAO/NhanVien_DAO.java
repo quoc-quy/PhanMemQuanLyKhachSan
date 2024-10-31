3597927531e5e648a960c663d1f93a19986ecc08
quoc-quy <quyleom10@gmail.com>
2024-10-31 13:53:41 +0700

    Làm trang Nhân Viên

--- src/main/java/DAO/NhanVien_DAO.java
+++ src/main/java/DAO/NhanVien_DAO.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import ConnectDB.ConnectDB;
-import ENTITY.KhachHang;
 import ENTITY.LoaiNhanVien;
 import ENTITY.NhanVien;
 
@@ -15,101 +10,95 @@ import java.util.List;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Admin
- */
 public class NhanVien_DAO {
-    private ConnectDB dbConnection = new ConnectDB();
-    
-//    kiểm tra coi có phải là Quản Lý 
-    public boolean isManager(String maTaiKhoan) {
-        boolean isManager = false;
-        Connection connection = dbConnection.getConnection();
-        try {
-            String query = "SELECT * FROM NHANVIEN WHERE TaiKhoan=? AND LoaiNhanVien='NV_QUANLY'";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, maTaiKhoan);
-            ResultSet rs = ps.executeQuery();
+	private final ConnectDB connectDB = new ConnectDB();
 
-            if (rs.next()) {
-                isManager = true;
-            }
-        } catch (SQLException e) {
-        }
-        return isManager;
-    }
-    
-//  kiểm tra coi có phải là Lễ tân
-    public boolean isReceptionist(String maTaiKhoan) {
-        boolean isReceptionist = false;
-        Connection connection = dbConnection.getConnection();
-        try {
-            String query = "SELECT * FROM NHANVIEN WHERE TaiKhoan=? AND LoaiNhanVien='NV_LETAN'";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, maTaiKhoan);
-            ResultSet rs = ps.executeQuery();
+	// Check if the user is a manager
+	public boolean isManager(String maTaiKhoan) {
+		boolean isManager = false;
+		try (Connection connection = ConnectDB.getConnection();
+				PreparedStatement ps = connection
+						.prepareStatement("SELECT * FROM NHANVIEN WHERE TaiKhoan=? AND LoaiNhanVien='NV_QUANLY'")) {
+			ps.setString(1, maTaiKhoan);
+			ResultSet rs = ps.executeQuery();
+			isManager = rs.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return isManager;
+	}
 
-            if (rs.next()) {
-                isReceptionist = true;
-            }
-        } catch (SQLException e) {
-        }
-        return isReceptionist;
-    }
-    
-//  Lấy tên nhân viên bằng mã tài khoản
-    public String getTenNhanVienByMaTaiKhoan(String maTaiKhoan) {
-        String tenNhanVien = "";
-        String query = "SELECT TenNhanVien FROM NhanVien WHERE TaiKhoan = ?";
-        
-        try (Connection conn = dbConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(query)) {
-            ps.setString(1, maTaiKhoan);
-            ResultSet rs = ps.executeQuery();
-            
-            if (rs.next()) {
-                tenNhanVien = rs.getString("TenNhanVien");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        return tenNhanVien;
-    }
-    
-//  lấy danh sách nhân viên
-    
-    public List<NhanVien> getAllNhanVien() {
-        List<NhanVien> dsNhanVien = new ArrayList<>();
-        String query = "SELECT * FROM NhanVien";
+	// Check if the user is a receptionist
+	public boolean isReceptionist(String maTaiKhoan) {
+		boolean isReceptionist = false;
+		try (Connection connection = ConnectDB.getConnection();
+				PreparedStatement ps = connection
+						.prepareStatement("SELECT * FROM NHANVIEN WHERE TaiKhoan=? AND LoaiNhanVien='NV_LETAN'")) {
+			ps.setString(1, maTaiKhoan);
+			ResultSet rs = ps.executeQuery();
+			isReceptionist = rs.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return isReceptionist;
+	}
 
-        try (Connection conn = dbConnection.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
+	// Get employee name by account ID
+	public String getTenNhanVienByMaTaiKhoan(String maTaiKhoan) {
+		String tenNhanVien = "";
+		String query = "SELECT TenNhanVien FROM NhanVien WHERE TaiKhoan = ?";
+		try (Connection conn = ConnectDB.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+			ps.setString(1, maTaiKhoan);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				tenNhanVien = rs.getString("TenNhanVien");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tenNhanVien;
+	}
 
-            while (rs.next()) {
-            	// Chuyển chuỗi lấy từ cơ sở dữ liệu thành enum
-                String loaiNhanVienStr = rs.getString("LoaiNhanVien");
-                LoaiNhanVien loaiNhanVien = LoaiNhanVien.valueOf(loaiNhanVienStr); 
-                
-                NhanVien nv = new NhanVien(
-                    rs.getString("MaNhanVien"),
-                    rs.getString("TenNhanVien"),
-                    loaiNhanVien,
-                    rs.getString("Phai"),
-                    rs.getDate("NgaySinh"),
-                    rs.getString("CCCD"),
-                    rs.getString("SDT")
-                );
-                dsNhanVien.add(nv);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return dsNhanVien;
-    }
-    
+	// Get all employees
+	public List<NhanVien> getAllNhanVien() {
+		List<NhanVien> dsNhanVien = new ArrayList<>();
+		String query = "SELECT * FROM NhanVien";
+
+		try (Connection conn = ConnectDB.getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				String loaiNhanVienStr = rs.getString("LoaiNhanVien");
+				LoaiNhanVien loaiNhanVien = LoaiNhanVien.valueOf(loaiNhanVienStr);
+				NhanVien nv = new NhanVien(rs.getString("MaNhanVien"), rs.getString("TenNhanVien"), loaiNhanVien,
+						rs.getString("Phai"), rs.getDate("NgaySinh"), rs.getString("CCCD"), rs.getString("SDT"));
+				dsNhanVien.add(nv);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return dsNhanVien;
+	}
+
+	// Get employee by ID
+	public NhanVien getNhanVienById(String maNhanVien) {
+		NhanVien nhanVien = null;
+		String query = "SELECT * FROM NhanVien WHERE MaNhanVien = ?";
+		try (Connection conn = ConnectDB.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setString(1, maNhanVien);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					String loaiNhanVienStr = rs.getString("LoaiNhanVien");
+					LoaiNhanVien loaiNhanVien = LoaiNhanVien.valueOf(loaiNhanVienStr);
+					nhanVien = new NhanVien(rs.getString("MaNhanVien"), rs.getString("TenNhanVien"), loaiNhanVien,
+							rs.getString("Phai"), rs.getDate("NgaySinh"), rs.getString("CCCD"), rs.getString("SDT"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return nhanVien;
+	}
 
 	// Add a new employee
 	public boolean addNhanVien(NhanVien nhanVien) {
