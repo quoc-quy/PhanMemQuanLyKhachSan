5ce78747037db2f4cc6e1210e6129483a287cbb4
quoc-quy <quyleom10@gmail.com>
2024-10-31 16:50:12 +0700

    làm chức năng Hủy đặt phòng

--- src/main/java/DAO/DanhSachDatPhong_DAO.java
+++ src/main/java/DAO/DanhSachDatPhong_DAO.java
@@ -1,6 +1,7 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,12 +11,15 @@ import java.util.List;
 import ConnectDB.ConnectDB;
 
 public class DanhSachDatPhong_DAO {
+	private final ConnectDB connectDB = new ConnectDB();
+	
+//	Lấy danh sách đặt phòng
 	public List<Object[]> getAllDanhSachDatPhong() {
 	    List<Object[]> danhSachDatPhong = new ArrayList<>();
 
 	    String sql = 
 	        "SELECT kh.TenKhachHang, kh.DenThoai, kh.Phai, " +
-	        "pdp.MaPhong, pdp.NgayNhanPhong, pdp.NgayTraphong, pdp.TienCoc " +
+	        "pdp.MaPhieuDatPhong, pdp.MaPhong, pdp.NgayNhanPhong, pdp.NgayTraphong, pdp.TienCoc " +
 	        "FROM PhieuDatPhong pdp " +
 	        "JOIN KhachHang kh ON pdp.MaKhachHang = kh.MaKhachHang ";
 
@@ -27,6 +31,7 @@ public class DanhSachDatPhong_DAO {
 	        while (rs.next()) {
 	            Object[] row = {
 	            	index,
+	                rs.getString("MaPhieuDatPhong"),
 	                rs.getString("TenKhachHang"),
 	                rs.getString("DenThoai"),
 	                rs.getString("Phai"),
@@ -45,4 +50,19 @@ public class DanhSachDatPhong_DAO {
 
 	    return danhSachDatPhong;
 	}
+
+ 	// Xóa đặt phòng
+ 	public boolean deletePhieuDatPhong(String MaPhieuDatPhong) {
+ 		String query = "DELETE FROM PhieuDatPhong WHERE MaPhieuDatPhong = ?";
+
+ 		try (Connection conn = connectDB.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+ 			pstmt.setString(1, MaPhieuDatPhong);
+
+ 			return pstmt.executeUpdate() > 0;
+ 		} catch (SQLException e) {
+ 			e.printStackTrace();
+ 		}
+ 		return false;
+ 	}
 }
